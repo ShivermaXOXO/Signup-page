ecb3caf50fca995a0f2d5d697033c5b141c1cb90
Shivam verma <vermashivam8868@gmail.com>
2023-12-26 13:53:35 +0530

    first commit

---
+++ createacc.js
@@ -0,0 +1,61 @@
+
+
+document.getElementById("showPassword").addEventListener("change", function () {
+    var passwordInput = document.getElementById("password");
+    var passwordRepeat = document.getElementById("psw-repeat");
+    passwordInput.type = passwordRepeat.type = this.checked ? "text" : "password";
+});
+
+document.getElementById("cancelbtn").addEventListener("click", function () {
+
+    var emailInput = document.getElementById("email");
+    var passwordInput = document.getElementById("password");
+    var repeatPassword = document.getElementById("psw-repeat");
+
+    emailInput.value = "";
+    passwordInput.value = "";
+    repeatPassword.value = "";
+});
+
+// function signup(){
+//     var password=document.getElementById("password").value;
+//     var repeatPassword=document.getElementById("psw-repeat").value;
+
+//     if (password === repeatPassword){
+//         alert("Successfully Signup");
+//     }else{
+//         alert("please try again");
+//     }
+// }
+function openPopup() {
+
+    popup.classList.add("open-popup");
+    setTimeout(closePopup, 1000);
+}
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
+function showPopup() {
+    openPopup();
+}
+// document.getElementById("signupbtn").addEventListener("click", function(){
+
+document.getElementById("psw-repeat").addEventListener("input", function () {
+    var password = document.getElementById("password").value;
+    // var repeatPassword = document.getElementById("psw-repeat").value;
+    
+    var repeatPassword = this.value;
+    var signupBtn = document.getElementById("signupbtn");
+
+    if (password === repeatPassword) {
+        showPopup();
+        signupBtn.style.display = "block";
+    } else {
+        
+        signupBtn.style.display = "none";
+    }
+});
+
+
+
